50c0e6d7e77a3fa35d3581db244ecaa34691ee0c
Alexandr Shatilov <ashatilov@webinar.ru>
2025-03-22 21:17:56 +0300

    DEV-5 Add test

---
+++ internal/infrastructure/server/app/apperror/status_error_test.go
@@ -0,0 +1,73 @@
+package apperror
+
+import (
+	"fmt"
+	"net/http"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestHTTPStatus(t *testing.T) {
+	type args struct {
+		err error
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{
+			name: "nil error",
+			args: args{
+				err: nil,
+			},
+			want: 0,
+		},
+		{
+			name: "not found error",
+			args: args{
+				err: StatusError{
+					error:  fmt.Errorf("not found"),
+					status: 404,
+				},
+			},
+			want: http.StatusNotFound,
+		},
+		{
+			name: "internal server error",
+			args: args{
+				err: StatusError{
+					error:  fmt.Errorf("internal server error"),
+					status: 500,
+				},
+			},
+			want: http.StatusInternalServerError,
+		},
+		{
+			name: "bad request",
+			args: args{
+				err: StatusError{
+					error:  fmt.Errorf("bad request"),
+					status: 400,
+				},
+			},
+			want: http.StatusBadRequest,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			assert.Equal(t, HTTPStatus(tt.args.err), tt.want)
+		})
+	}
+}
+
+func TestStatusError_HTTPStatus(t *testing.T) {
+	e := StatusError{
+		error:  fmt.Errorf("internal server error"),
+		status: 500,
+	}
+
+	assert.Equal(t, e.Unwrap(), fmt.Errorf("internal server error"))
+	assert.Equal(t, e.HTTPStatus(), 500)
+}
